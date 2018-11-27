f85b531ed5c0a59140d1f9c236a39a9dc05db4f5
Simon Thome <s.thome@marcobot.io>
2018-11-27 14:59:16 +0100

    first draft buttons

--- src/Components/buttonArray/buttonArray.js
+++ src/Components/buttonArray/buttonArray.js
@@ -4,15 +4,22 @@ import React from 'react';
 export default function ButtonArray(props) {
   return (
     <div className="buttonArrayContainer">
-    {props.array.map(item => {
-      return (
-        <button className="buttonArray">
-          <label className="buttonArrayTitle">Pack {item.name}</label>
-          <label className="buttonArrayPrice">{item.price}€</label>
+      {props.array.map(item => {
+        return (
+          <div className="buttonContainer">
+            <button className="buttonArray">
+              <label className="buttonArrayTitle">Pack {item.name}</label>
+              <label className="buttonArrayPrice">{item.price}€</label>
+            </button>
+          </div>
+        )
+      })
+      }
+      <div className="buttonContainerOther">
+        <button className="buttonArrayOther">
+          <label className="buttonArrayTitle">Autre</label>
         </button>
-      )
-    })
-    }
+      </div>
     </div>
   )
 }
